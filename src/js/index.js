2c20731d24c9af288f7980da8bb2e1f18c5202d3
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2024-01-17 12:33:43 +0300

    gulp

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+import autoComplete from './modules/autoComplete';
+autoComplete();
+
+import easepick from './modules/easepick.js';
+easepick();
+
+import swiper from './modules/swiper-popular.js';
+swiper();
+
+import swiperTestimonials from './modules/swiper-testimonials.js';
+swiperTestimonials();
+
+import scrollReveal from './modules/scrollReveal.js';
+scrollReveal();
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
